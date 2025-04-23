567e617f8d57269c8f837335ff9512d28a8ba341
donggyulee <nninnnin7@gmail.com>
2025-04-23 23:30:53 +0900

    메인 스크립트 정리

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a-scene>
-    </a-scene>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+console.log("Main Script");
