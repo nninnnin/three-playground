c8109fde389ef9742199ecf6d1c2613a5c254757
donggyulee <nninnnin7@gmail.com>
2025-04-23 23:26:07 +0900

    박스 제거, VR 버튼 제거

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
   <div>
     <a-scene>
-      <a-box position="0 1 -4" rotation="0 45 0" color="#4CC3D2"></a-box>
     </a-scene>
   </div>
 `;
